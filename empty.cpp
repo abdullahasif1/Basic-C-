488f93c67fe12f0b8ea13e680b2d85bba9d23444
abdullahasif1 <asifabdullah63@yahoo.com>
2020-07-13 00:53:41 +0500

    lab2 task1

---
+++ empty.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+
+printf("New repo\n\n\n");
+
+}
---
+++ task2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+ double x,y,z;
+
+cout<<"Sum of -9 and 2.5 is  "<<"-9+2.5="<<(-9+2.5)=x<<endl;
+cout<<"Difference between -9 from 2.5 is  "<<"-9-(2.5)="<<(-9-(2.5))=x<<endl;
+cout<<"Product of -9 and 2.5 is  "<<"-9*2.5="<<
+
+
+
+
+
+return 0;
