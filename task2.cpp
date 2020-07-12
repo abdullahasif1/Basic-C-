115f3a5878d38dc9b179cd9281f700324a02972d
abdullahasif1 <asifabdullah63@yahoo.com>
2020-07-13 01:09:23 +0500

    lab2 task1 fixed a bug

--- task2.cpp
+++ task2.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 int main(){
 
- double x,y,z;
-
-cout<<"Sum of -9 and 2.5 is  "<<"-9+2.5="<<(-9+2.5)=x<<endl;
-cout<<"Difference between -9 from 2.5 is  "<<"-9-(2.5)="<<(-9-(2.5))=x<<endl;
-cout<<"Product of -9 and 2.5 is  "<<"-9*2.5="<<
+ double x;
+x=(-9+2.5);
+cout<<"Sum of -9 and 2.5 is  "<<"-9+2.5="<<x<<endl;
+x=(-9-(2.5));
+cout<<"Difference between -9 from 2.5 is  "<<"-9-(2.5)="<<x<<endl;
+x=(-9*2.5);
+cout<<"Product of -9 and 2.5 is  "<<"-9*2.5="<<x;
 
 
 
 
 
 return 0;
+}
