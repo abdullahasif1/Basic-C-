c86823170993ed2f9d5c7b8e6d47ec2a4a9e4993
abdullahasif1 <asifabdullah63@yahoo.com>
2020-07-13 23:39:55 +0500

    ex4task3

---
+++ ex4task1lab4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main(){
+double x;
+const double a=8,b=11.5,c=-2,d=-7;
+x=0.45;
+cout<<x;
+ x=2.0*a+4*b-9*c+15*d+0.3;
+cout<<"First expression is equal to "<<x<<endl;
+ x=(4*b-5*c)/(7*a+b-2*d);
+cout<<"Second expression is equal to "<<x<<endl;
+ x=(sqrt(5*d+3*a*c) * sqrt(c+4*a*b))/ 2*a;
+cout<<"Third expression is equal to  "<<x<<endl;
+ x=(4*(a+2*b)*(5*c-2*d))/(9*(43+11*a));
+cout<<"Forth expression is equal to  "<<x<<endl;
+
+
+
+return 0;
+}
---
+++ ex4task3.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main(){
+
+int a;
+string x,y;
+cout<<"Enter your first name: ";
+cin>>x;
+
+cout<<"Enter your second name: ";
+cin>>y;
+
+cout<<"Enter your age : ";
+cin>>a;
+
+cout<<"Your gmail address is : "<<x<<y<<a<<"@gmail.com"<<endl;
+
+
+
+
+
+
+
+return 0;
+}
