b3d8c52c5a0d913d470affb6a724c6dbab736519
abdullahasif1 <asifabdullah63@yahoo.com>
2020-07-16 03:06:16 +0500

    lab6task4

---
+++ lab6task4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cout<<"Enter the date of October 2018: ";
+cin>>x;
+
+switch(x){
+   case 7:
+    cout<<"Its a Sunday";
+    break;
+   case 14:
+    cout<<"Its a Sunday";
+    break;
+   case 21:
+    cout<<"Its a Sunday";
+    break;
+   case 28:
+    cout<<"Its a Sunday";
+    break;
+default:
+cout<<"Its not a Sunday";
+
+
+}
+
+return 0;
+}
